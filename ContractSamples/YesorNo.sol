0de0cfc12d80e3aa34f76a71abdf9bc9d8a998a3
ImmutableDebate <47071980+ImmutableDebate@users.noreply.github.com>
2019-01-27 13:39:49 -0500

    Update YesorNo.sol

--- ContractSamples/YesorNo.sol
+++ ContractSamples/YesorNo.sol
@@ -12,11 +12,11 @@ contract YesorNo{
   event Argument(bool b, arguer arg);
   
   function sendEvent(bool _argum, string _n) public returns (bool) {
-    require(voted == false);
     arguer newArguer = arguer({
 	name: _n;
 	});
 	arguers[msg.sender] = newArguer;
+	require(newArguer.voted == false);
 	emit Argument(_argum, arguers[msg.sender]); 
 	return _argum;
   }
