689c91ea32964c3025e25b62641d39b45c350a77
ImmutableDebate <47071980+ImmutableDebate@users.noreply.github.com>
2019-01-26 23:27:05 -0500

    Add files via upload

--- Debate.sol
+++ Debate.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.5.0;
 
 contract Debate {
 
-  event Argument(string a);
+  event Argument(string a, address ad);
   
   function sendEvent(string _argum) public { 
-	emit Argument(_argum); 
+	emit Argument(_argum, msg.sender); 
 	return argum;
   }
 
