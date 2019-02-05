dade670420eb07ca9be7e357533d126e3658d831
ImmutableDebate <47071980+ImmutableDebate@users.noreply.github.com>
2019-02-04 23:10:29 -0500

    Update Debate.sol

--- ContractSamples/Debate.sol
+++ ContractSamples/Debate.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.5.0;
 
+import './Ownable.sol';
+
 contract Debate is Ownable{
 
   struct arguer{
@@ -8,20 +10,21 @@ contract Debate is Ownable{
   }
   
   mapping(address => arguer) arguers;
-  event Argument(string a, bool c, uint8 conf, arguer arg);
+  event Argument(string a, bool c, uint8 conf);
   event PossibleChangePending(string cp, address a); 
   
-  function sendEvent(string _argum, string _n, bool _change, uint8 _confidence) public returns (string, string, bool, uint8) onlyOwner{ 
-    arguer newArguer = arguer({
-	name: _n;
+  function sendEvent(string memory _argum, string memory _n, bool _change, uint8 _confidence) public onlyOwner returns (string memory, string memory, bool, uint8) { 
+    arguer memory newArguer = arguer({
+	name: _n,
+	department: ''
 	});
 	arguers[msg.sender] = newArguer;
 	//emit argument with statement,whether a change of opinion occurred, and address
-	emit Argument(_argum, _change, _confidence, arguers[msg.sender]);
-	return _argum,_n,_change,_confidence; 
+	emit Argument(_argum, _change, _confidence);
+	return (_argum,_n,_change,_confidence); 
   }
 	
-  function PostChange() public isOwner
+  function PostChange() public onlyOwner
  {
     emit PossibleChangePending("User might change decision.", msg.sender); 
 
