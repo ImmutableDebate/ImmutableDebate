f1975950f1550089d37bc15e227c3bff272a4312
ImmutableDebate <47071980+ImmutableDebate@users.noreply.github.com>
2019-03-11 12:24:05 -0400

    Update javascript.js

--- Web3jsSamples/javascript.js
+++ Web3jsSamples/javascript.js
@@ -1,3 +1,6 @@
+const Web3 = require('web3');
+const web3 = new Web3("https://mainnet.infura.io/v3/{infuraID goes here}");
+//const web3 = new Web3("https://ropsten.infura.io/v3/{infuraID goes here}");
 
 const Debate = new web3.eth.Contract(abi, address);
 Debate.methods.sendEvent("Hope so.", "Smith").call()
